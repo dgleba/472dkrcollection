9c2b812f270b64df3b6e27c1d09c9384b6b96fd1
albe <albe@ubu398j.local>
2020-03-03 09:29:22 -0500

    2020-03-03f

--- sc4511/sc4/index.js
+++ sc4511/sc4/index.js
@@ -1,14 +1,38 @@
 
 const puppeteer = require('puppeteer');
 const dayjs = require('dayjs');
+require('dotenv').config()
+
+// mail..
+//
+var nodemailer = require('nodemailer');
+// var smtpTransport = require('nodemailer-smtp-transport');
+
+var transporter = nodemailer.createTransport({
+  host: "smtp-mail.outlook.com", // hostname
+  secureConnection: false, // TLS requires secureConnection to be false
+  port: 587, // port for secure SMTP
+  tls: {
+    ciphers: 'SSLv3'
+  },
+  auth: {
+    user: process.env.ev_hotemail,
+    pass: process.env.ev_hotpass
+  }
+});
+
 
 // handle promise errors globally..
 // https://stackoverflow.com/questions/31472439/catch-all-unhandled-javascript-promise-rejections
+//
 process.on('unhandledRejection', function (err, promise) {
   console.error('Unhandled rejection (promise: ', promise, ', reason: ', err, ').');
   return process.exit(1);
   // dgleba_make_error_to_exit();
 });
+
+
+//  look for.. 2020-2021
 
 (async () => {
   const url = 'https://sc4.edu/campus-life/college-housing/college-housing-application/';
@@ -24,9 +48,38 @@ process.on('unhandledRejection', function (err, promise) {
       text
     );
     console.log("SUCCESS ------------------- text found. Check the page your self now.");
+    
+    var mailOptions = {
+      from: 'davidgleba@hotmail.com',
+      to: 'dgleba@gmail.com',
+      subject: '-- SUCCESS -- new application found',
+      text: 'The text 2020-2021 was found, so you should visit the page yourself and see it is true.'
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });     
+    
     await page.screenshot({ path: '/app/output/pagefound.png', fullPage: true });
   } catch (e) {
     console.log(`error.. The text "${text}" was NOT NOT NOT on the page`);
+
+    var mailOptions = {
+      from: 'davidgleba@hotmail.com',
+      to: 'dgleba@gmail.com',
+      subject: 'error - sc4 housing new application NOT found',
+      text: 'the text `2020-2021` was NOT there'
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });    
     // await page.screenshot({path: '/app/output/page-textnotfound.png', fullPage:true});
   }
 
@@ -50,6 +103,21 @@ process.on('unhandledRejection', function (err, promise) {
       text
     );
     console.log("SUCCESS ------------------- text found. Check the page your self now.");
+    
+    var mailOptions = {
+      from: 'davidgleba@hotmail.com',
+      to: 'dgleba@gmail.com',
+      subject: '-- SUCCESS -- new application found',
+      text: 'The text 2021 was found, so you should visit the page yourself and see it is true.'
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+    
     await page.screenshot({ path: '/app/output/pagefound2021.png', fullPage: true });
   } catch (e) {
     console.log(`error.. The text "${text}" was NOT NOT NOT on the page`);
