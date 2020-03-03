e3a707b13358630516d77a63c2bb886446424f05
albe <albe@ubu398j.local>
2020-03-03 12:38:53 -0500

    2020-03-03ha

---
+++ sc4511/sc4/test-email.504dc.js
@@ -0,0 +1,108 @@
+
+require('dotenv').config()
+
+var nodemailer = require('nodemailer');
+// var smtpTransport = require('nodemailer-smtp-transport');
+
+
+// 2020-03-03 12:20PM works.
+//
+var transporter = nodemailer.createTransport({
+  host: "smtp-mail.outlook.com", // hostname
+  secureConnection: false, // TLS requires secureConnection to be false
+  port: 587, // port for secure SMTP
+  tls: {
+    ciphers: 'SSLv3'
+  },
+  auth: {
+    user: process.env.ev_hotemail,
+    pass: process.env.ev_hotpass
+  }
+});
+
+
+// 504dc
+// 
+// https://nodemailer.com/smtp/
+// secure – if true the connection will use TLS when connecting to server. If false (the default) then TLS is used if server supports the STARTTLS extension. In most cases set this value to true if you are connecting to port 465. For port 587 or 25 keep it false.
+
+var transporter = nodemailer.createTransport({
+  host: "mail.jgleba.com", // hostname
+  secure: false, // TLS requires secureConnection to be false
+  port: 25 // port for secure SMTP
+  
+});
+
+
+// var smtpTransport = nodemailer.createTransport(smtpTransport({
+//   service: 'Hotmail',
+//   auth: {
+//     user: process.env.ev_hotemail,
+//     pass: process.env.ev_hotpass
+//   }
+// }));
+
+
+// var transporter = nodemailer.createTransport({
+//   service: "Hotmail"{
+//   host: "smtp-mail.outlook.com",
+//   port: 587,
+//   requiresAuth: true,
+//   domains: ["hotmail.com", "outlook.com"],
+//   tls: { ciphers: 'SSLv3' }
+// },
+// });
+
+
+// var transport = nodemailer.createTransport("SMTP", {
+// host: "smtp-mail.outlook.com",
+// secureConnection: false, 
+// port: 587, 
+// auth: {
+// user: "davidgleba@hotmail.com",
+// pass: "x"
+// },
+// tls: {
+// ciphers:'SSLv3'
+// }
+// });
+
+
+// const transporter = nodemailer.createTransport({
+// host: 'smtp-mail.outlook.com',
+// service:'smtp-mail.outlook.com',
+// port: 587,
+// secure: false,  //true for 465 port, false for other ports
+// auth: {
+// user: 'davidgleba@hotmail.com',
+// pass: 'x'
+// },      
+// secureConnection: false,
+// tls: {
+// ciphers: 'SSLv3',
+// rejectUnauthorized: true
+// }
+// });    
+
+
+// nodemailer.createTransport({
+//   host: "MESG01",
+//   secure: false, // upgrade later with STARTTLS
+// });
+
+
+var mailOptions = {
+  from: 'j@jgleba.com',
+  to: 'dgleba@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was not easy! 09:01AM'
+};
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
+
