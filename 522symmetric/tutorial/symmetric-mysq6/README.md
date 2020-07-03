eb4a4a0a0ebd8d6c919ae1ec623ee119be01aeba
David Gleba <dgleba@gmail.com>
2020-07-03 18:21:56 -0400

    2020-07-03

--- 522symmetric/tutorial/symmetric-mysq6/README.md
+++ 522symmetric/tutorial/symmetric-mysq6/README.md
@@ -5,13 +5,18 @@
 # Instructions.
 
 ```
+
+1.
 # may be optional.
 chmod +x conf/*.sh
 
+2.
 docker-compose build
 
+3.
 docker-compose up 
 
+4.
 
 # Initial load store node with data from corp
 #
@@ -19,10 +24,30 @@ docker-compose up
 #
 docker-compose exec sym bash -c 'syms/bin/symadmin --engine corp-000 reload-node 001'
 
+It should say.. `Successfully enabled initial load for node 001`
+If not, then try again in a minute. I might not be ready yet.
 
-Done. Now edits in corp show up in store1.
+5.
+Done. Now edits in item table in corp show up in store1.
+
+
+6.
+Use adminer to view the mysql databases 
+
+visit hostip:14119 to use adminer in your browser.
+
+use:
+system: mysql
+server: corp or store1
+username: root
+password: user
+
 
 ```
+
+
+
+
 
 # More.
 
@@ -32,6 +57,9 @@ Done. Now edits in corp show up in store1.
 docker-compose exec sym bash -c 'tail -f syms/logs/symmetric.log'
 
 ```
+
+
+
 
 
 # Troubleshooting.
---
+++ 522symmetric/tutorial/symmetric-mysq6/conf2/get-tables.sql.sh
@@ -0,0 +1,34 @@
+
+------------------------------------------------------------------------------
+-- get list of all tables.
+------------------------------------------------------------------------------
+
+
+SELECT table_name FROM information_schema.tables
+WHERE table_schema = 'itemdb' and table_name not like 'SYM_%';
+
+
+-- =================================================
+
+
+-- Create sql by pasting into bash prompt ..
+
+
+array21=( 
+item
+qissues
+test                             
+) 
+date
+for patrn in "${array21[@]}"
+do
+    echo  "
+      insert into sym_trigger 
+            (trigger_id,  source_table_name,        channel_id,    last_update_time,  create_time)
+      values('"${patrn}"',    '"${patrn}"',            'main0',    current_timestamp, current_timestamp);
+      insert into sym_trigger_router 
+            (trigger_id,router_id,     initial_load_order, last_update_time,  create_time)
+      values('"${patrn}"',    'corp_2_store',   100,             current_timestamp, current_timestamp);
+      ----------------"
+done
+
